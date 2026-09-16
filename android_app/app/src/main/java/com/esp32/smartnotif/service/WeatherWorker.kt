039ff841e53fcd804670c48400325ff6fd800bf4
HiuBodas <umam8364@gmail.com>
2026-09-16 10:56:58 +0700

    fix(worker): import androidx.work.ListenableWorker.Result to fix Kotlin compiler ambiguity

--- android_app/app/src/main/java/com/esp32/smartnotif/service/WeatherWorker.kt
+++ android_app/app/src/main/java/com/esp32/smartnotif/service/WeatherWorker.kt
@@ -6,6 +6,7 @@ import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ExistingWorkPolicy
+import androidx.work.ListenableWorker.Result
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.PeriodicWorkRequest
